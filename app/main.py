a5c0b48921e313648e7979629cdacb2d53f21095
Ricky <myathein060@gmail.com>
2025-07-30 22:09:41 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-import socket  # noqa: F401
+import socket
+from sqlite3 import Connection  # noqa: F401
 
 
 def main():
@@ -8,9 +9,24 @@ def main():
     #Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].send(b"HTTP/1.1 200 OK\r\n\r\n")
-    server_socket.accept() # wait for client
+    connection, address = server_socket.accept()
+    print("Client connected")
+    
+    request =connection.recv(1024).decode()
 
+    request_lines = request.split(" ")
+    method = request_lines[0]
+    path = request_lines[1]
+    version = request_lines[2]
+
+    if path == "/":
+        response = "HTTP/1.1 200 OK\r\n"
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n"
+
+    print(response)
+    connection.sendall(response.encode())
+    connection.close()
 
 if __name__ == "__main__":
     main()
