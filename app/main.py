f628ebd1d43a46e454ffbf3ec2e0a0aca966c50c
Ricky <myathein060@gmail.com>
2025-07-31 12:04:02 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -11,34 +11,42 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept()
     
-    request =connection.recv(1024).decode()
+    request = connection.recv(1024).decode()
 
     lines = request.split("\r\n")
-    print(lines)
     request_line = lines[0]
-    headers = lines[1:]
-
-    parts = request_line.split(" ")
-    method = parts[0]
-    path = parts[1]
+    method, path, version = request_line.split(" ")
 
     user_agent = ""
-    for header in headers:
-        if header.lower().startswith("user-agent:"):
-            user_agent = header.split(": ")[1]
-            break
+    for line in lines:
+        if line.startswith("User-Agent: "):
+            user_agent = line[len("User-Agent: ")]
 
-    if method == "GET" and path == "/user-agent":
-        content_length = len(user_agent)
-        response = (
-                "HTTP/1.1 200 OK\r\n"
+    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    
+    if method == "GET":
+        if path == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif path == "/user-agent":
+            content_length = len(user_agent.encode())
+            response = (
+                "HTTP/1.1 200 OK\r\n\r\n"
                 "Content-Type: text/plain\r\n"
                 f"Content-Length: {content_length}\r\n"
                 "\r\n"
                 f"{user_agent}"
             )
-    else:
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        elif path.startswith("/echo/"):
+            echo_string = path[len("/echo/"):]
+            content_length = len(echo_string.encode())
+            response = (
+                "HTTP/1.1 200 OK\r\n\r\n"
+                "Content-Type: text/plain\r\n"
+                f"Content-Length: {content_length}\r\n"
+                "\r\n"
+                f"{echo_string}"
+            )
+
     connection.sendall(response.encode())
     connection.close()
 
